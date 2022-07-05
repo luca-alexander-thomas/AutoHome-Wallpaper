2f2a71aafeb4bf0a46ff35489f42a1d967aad8e4
Luca-Alexander Thomas <mail@lucathomas.de>
2022-07-05 17:44:33 +0200

    basic script

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import json
+from urllib.request import urlopen
 import os
 import sys
 import time
 
 
-with open('wallpaper.json') as f:
-    data = json.load(f)
+with urlopen('https://raw.githubusercontent.com/luca-alexander-thomas/AutoHome-Wallpaper/main/wallpaper.json') as response:
+    source = response.read()
+
+raw_data = json.loads(source)
+data = json.dumps(raw_data, indent=4, sort_keys=True)
+print(data)
 
